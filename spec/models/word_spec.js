97d0d9417d216fc3a1d267d6c0d678862272b2a5
CheezItMan <chris@adadevelopersacademy.org>
2017-12-08 14:33:25 -0800

    Updated specs to ES2015

--- spec/models/word_spec.js
+++ spec/models/word_spec.js
@@ -2,66 +2,76 @@
 import _ from 'underscore';
 import Word from 'models/word';
 
-describe('Word', function() {
-  describe('constructor', function() {
-    it('Tracks text', function() {
+describe('Word', () => {
+  describe('constructor', () => {
+    it('Tracks text', () => {
       var word = new Word({
         text: "test"
       });
+
       expect(word.get('text')).toEqual('test');
     });
 
-    it ('Converts text to lowercase', function() {
-      var word = new Word({ text: 'TeSt' });
+    it ('Converts text to lowercase', () => {
+      const word = new Word({ text: 'TeSt' });
+
       expect(word.get('text')).toEqual('test');
     });
   });
 
-  describe('validate', function() {
-    it ('permits valid words', function() {
+  describe('validate', () => {
+    it ('permits valid words', () => {
+
       _.each(['f', 'dog', 'pig', 'goat', 'swizzle'], (text) => {
-        var word = new Word({ text: text });
+        const word = new Word({ text: text });
+
         expect(word.isValid()).toBeTruthy('word: ' + text + ', error: ' + word.validationError);
       });
     });
 
-    it ('permits upper- and lower-case letters', function() {
+    it ('permits upper- and lower-case letters', () => {
       _.each(['dog', 'DOG', 'DoG'], (text) => {
-        var word = new Word({ text: text });
+        const word = new Word({ text: text });
+
         expect(word.isValid()).toBeTruthy('word: ' + text + ', error: ' + word.validationError);
       })
     })
 
-    it ('requires text', function() {
-      var word = new Word();
+    it ('requires text', () => {
+      const word = new Word();
+
       expect(word.isValid()).toBeFalsy();
     });
 
     // DPR: This is a little pedantic, but it's something I
     // actually ran into while implementing the front end
     // so before fixing the problem I added a test
-    it ('requires text to be a string', function() {
-      var word = new Word({ text: 333 });
+    it ('requires text to be a string', () => {
+      const word = new Word({ text: 333 });
+
       expect(word.isValid()).toBeFalsy();
     });
 
-    it ('rejects empty words', function() {
-      var word = new Word({ text: '' });
+    it ('rejects empty words', () => {
+      const word = new Word({ text: '' });
+
       expect(word.isValid()).toBeFalsy();
     });
 
-    it ('rejects words > 7 letters', function() {
+    it ('rejects words > 7 letters', () => {
       // Boundary condition: 7 letters should work,
       // 8 should fail
-      var word = new Word({ text: 'abcdefg' });
+      let word = new Word({ text: 'abcdefg' });
+
       expect(word.isValid()).toBeTruthy();
 
-      var word = new Word({ text: 'abcdefgh' });
+      word = new Word({ text: 'abcdefgh' });
+
       expect(word.isValid()).toBeFalsy();
     });
 
-    it ('rejects things other than letters', function() {
-      var words = [
+    it ('rejects things other than letters', () => {
+      const words = [
         new Word({ text: '!@#$' }),
         new Word({ text: 'aa&bb' }),
         new Word({ text: '123' })
@@ -72,18 +82,17 @@ describe('Word', function() {
     });
   });
 
-  describe('score', function() {
-    it ('Correctly scores simple words', function() {
+  describe('score', () => {
+    it ('Correctly scores simple words', () => {
       // TODO
     });
 
-    it ('Adds 50 points for a 7-letter word', function() {
+    it ('Adds 50 points for a 7-letter word', () => {
       // TODO
     });
 
-    it ('Returns undefined if the word is invalid', function() {
+    it ('Returns undefined if the word is invalid', () => {
       // TODO
     });
   });
 });
-
